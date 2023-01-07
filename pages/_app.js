5802440d6f1272a6da674d4e433d2d02316af365
Smital-Lunawat <lunawatsmital@gmail.com>
2023-01-08 00:41:17 +0530

    Changes in UI

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
+  <Head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
+  </Head>
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import axios from 'axios';
 
@@ -39,6 +40,7 @@ export default function Home() {
       // return String(text);
       // console.log("My Result: ", currentPrompt);
       setIsExtracting(false);
+      moveToSummary();
 		} 
      
       catch (err) {
@@ -82,9 +84,9 @@ export default function Home() {
   };
 
   const renderButton = () => {
-    if(isExtracting) {
-      return <h2>Extracting the text for you... Please wait..</h2>;
-    }
+    // if(isExtracting) {
+    //    return <h2>Extracting the text for you... Please wait..</h2>;
+    // }
     if (loading) {
       return <h2>Summarizing the text for you... Please wait..</h2>;
     } else {
@@ -100,13 +102,30 @@ export default function Home() {
               Input Prompt: {currentPrompt}
             </div> */}
             <div className={styles.description}>
-              Summary (TL;DR):{summaryResult}
+            <span style={{color: '#45424b'}}> Summary</span> {summaryResult}
             </div>
           </div>
         </div>
       );
     }
   };
+  const myDiv = useRef(null)
+
+  const handleClick = () => {
+    myDiv.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
+  const summary = useRef(null)
+
+  const moveToSummary = () => {
+    summary.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    })
+  }
+  
 
   return (
     <div>
@@ -115,7 +134,104 @@ export default function Home() {
         <meta name="description" content="TLDR-Summarizer" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.main}>
+      <div className={styles.overlay}></div>
+      <div className={styles.big_wrapper}>
+      <div className={styles.showcase_area}>  
+            <div className={styles.left}>
+              <div className={styles.big_title}>
+                <h1>Quick <span style={{color: '#5761e7'}}> Summaries,</span></h1>
+                <h1>Big Time Saving!</h1>
+              </div>
+              <p className={styles.text}>
+              <strong>Do you struggle to keep up with long articles, emails, or reports?</strong> <br></br>Our summarization app makes it easy to quickly get the main points of any text or link. Simply input the content and our app will generate a condensed summary in just a few moments.
+              </p>
+              <button className={styles.button} onClick={handleClick}> Try it! </button>
+            </div>
+            
+            <div className={styles.right}>
+              <Image src="/person.svg" className={styles.person} width="200" height="500"></Image>
+            </div>  
+          </div>  
+          <hr className={styles.hr}></hr>
+<div className={styles.hero}>
+  <h1>Key benefits of<span style={{color: '#5761e7'}}> TL;DR</span> </h1> 
+  <ul className={styles.cards}>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_image}>
+          {/* <img className={styles.img} src="https://picsum.photos/500/300/?image=10"/>  */}
+          <Image src="/cpu-3.png" className={styles.img} width="230" height="10"></Image>
+        </div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>AI-powered summarization</h2>
+          <p className={styles.card_text}> Our app uses advanced machine learning and algorithms to identify and extract the most important information from your text.</p>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_image}>
+          <Image src="/click.png" className={styles.img} width="230" height="10"></Image>
+
+        </div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Easy to use</h2>
+          <p className={styles.card_text}>The app should have a simple and intuitive interface that makes it easy for users to input texts and links and view summaries.</p>
+        </div>
+      </div>
+    </li>
+    <li className={styles.cards_item}>
+      <div className={styles.card}>
+        <div className={styles.card_image}>
+          <Image src="/deadline.png" className={styles.img} width="230" height="10"></Image>
+
+       </div>
+        <div className={styles.card_content}>
+          <h2 className={styles.card_title}>Fast and Efficient</h2>
+          <p className={styles.card_text}>Our app uses advanced algorithms to quickly and efficiently summarize texts and links, so you can get your summaries in just a few seconds.</p>
+          </div>
+        </div>
+    </li>
+  </ul>
+</div>
+<hr  className={styles.hr}></hr>
+  <div className={styles.form}>
+          <div>
+          <h1 ref={myDiv} className={styles.title}>Enter the text to summarize!</h1>
+          </div>
+    <fieldset className={styles.fieldset}>
+      <label className={styles.label} for="name">Paste The Article Link Here...</label>
+      <input className={styles.input} type="text" onChange={e=> {
+            //setIsExtracting(true);
+            setUrl(e.target.value);
+          }}/>
+       <button className={styles.button} onClick={()=> {fetchExtractedContent();}}>
+              Extract From URL!
+       </button>
+       {
+         isExtracting?<h2>Extracting the text for you... Please wait..</h2>:null
+       }
+       
+      <h2>OR</h2>
+      <br></br>
+
+      <label className={styles.label} for="bio">Your Article Text Goes Here...</label>
+      <textarea  rows="20" cols="80"className={styles.input} 
+
+            onChange={e=>{
+              // setIsExtracting(false);
+              setCurrentPrompt(e.target.value);
+            }} 
+            value = {currentPrompt}
+            ref={inputPrompt}>
+
+      </textarea>
+      
+    </fieldset> 
+    {renderButton()}
+  </div>
+      
+      {/* <div className={styles.main}>
         <div>
           <h1 className={styles.title}>Enter the text to summarize!</h1>
           <div className={styles.description}>
@@ -155,10 +271,12 @@ export default function Home() {
           </div>
           {renderButton()}
         </div>
-      </div>
-      <footer className={styles.footer}>
+      </div> */}
+      <footer ref={summary} className={styles.footer}>
         Made by Eesh & Smital for NWS2 &#10084;
       </footer>
+      </div>  
     </div>
+
   );
 }
