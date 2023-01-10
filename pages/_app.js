c5c6d08d86fa58a74db397ddba6c6569e7160a1b
eesh <eesh1996@gmail.com>
2023-01-10 16:51:06 +0530

    added analytics to application

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,18 @@
-import '../styles/globals.css'
-import Head from 'next/head'
+import "../styles/globals.css";
+import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   <Head>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" />
-  </Head>
-  return <Component {...pageProps} />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    />
+  </Head>;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
